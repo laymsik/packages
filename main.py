9d30a61b9d382a2da486775666ace06c39870bc6
laymsik <layms1k0987@gmail.com>
2025-02-01 18:11:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,51 +2,34 @@ import subprocess
 
 packages = \
 ["swaybg", #Основные пакеты!
-"kiity",
+"foot",
 "waybar",
 "fish",
-"wofi",
-"git",
-"thunar",
-"zenity",
-"figlet",
+"rofi",
 "nwg-look",
+"hyprlock",
 "gtk2",
 "gtk3",
-"gtk4",
-"neofetch",
 "w3m",
 "sddm",
 "qt5",
+"mako",
+
 #Шрифты !
-"ttf-dejavu",
-"ttf-font-awesome",
-"ttf-liberation",
-"ttf-dejavu ",
-"powerline-fonts",
-"noto-fonts",
-"ttf-fira-code",
-"ttf-hack",
-"terminus-font",
-"adobe-source-code-pro-fonts",
-"otf-font-awesome",
+
 "ttf-jetbrains-mono",
-"nerd-fonts-complete",
-"ttf-fira-code-nerd",
-"powerline-fonts",
-"ttf-google-fonts-git",
-"nerd-fonts",
-"fc-cache -fv",
- "ttf-fira-mono",
-#--------------------
-"noto-fonts-emoji",
-"ttf-joypixels",
+"breeze",
+
 #Утилиты
+
 "inetutils",
 "pkgfile",
 "base-devel",
 "grim",
+"catppuccin",
+
 #Обновления
+
 "sudo pacman -Syu"
 
 ]
